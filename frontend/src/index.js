f2be00a14065ad1030fbdfb129890b19e54d391d
Theo Weller <weller.theodor@gmail.com>
2019-08-12 17:48:32 -0400

    goodreads app moved from old repository and consolidated frontend and backend into one app

---
+++ frontend/src/index.js
@@ -0,0 +1,81 @@
+$( document ).ready(() => {
+$("#book-container").toggleClass("hide")
+/**************
+EVENT HANDLERS
+***************/
+//Search button event listener
+$("#search-container button").click(e => {
+  newSearchHandler()
+  const query = $("#search").val()
+  fetchHandler(query)
+});
+
+//Book container event listener
+$('#book-container').on('click', '.book-container-item', (e) => {
+  if(e.target.tagName !== "UL"){
+    showBookFetchHandler(e.target.parentElement.id)
+  } else {
+    showBookFetchHandler(e.target.id)
+  };
+});
+
+//exit popup handler
+$("#popup").click(e => {
+  $("#popup").toggleClass("activate")
+  $("#popup-content").remove()
+});
+
+/**************
+HELPER METHODS
+***************/
+//add book items to dom
+const appendBooks = (books) => {
+  books.forEach(book => {
+    $( "#book-container" ).append(`
+        <ul class="book-container-item" id=${book.id}>
+          <img src=${book.image_url}>
+          <span>${book.title}</span>
+          <span class="author">Author: ${book.author.name}</span>
+        </ul>
+      `
+    );
+  });
+};
+
+const appendBookToPopup = (book) => {
+  $( "#popup" ).append(`
+    <div id="popup-content">
+      <img src=${book.image_url}>
+      <span><a href=${book.url}>${book.title}</a></span>
+      <span class="author">Author: ${book.author}</span>
+      <span>${book.description}</span>
+    </div>
+    `
+  );
+  $("#popup").toggleClass("activate")
+  $("#book-container").toggleClass("hide")
+};
+
+const newSearchHandler = () => {
+  $(".book-container-item").length >= 0 && $(".book-container-item").remove();
+};
+
+const fetchHandler = query => {
+  fetch(`http:localhost:3000/api/v1/books/${query}`)
+  .then(response => response.json())
+  .then(data => {
+    appendBooks(data);
+    $("#book-container").toggleClass("hide")
+  });
+};
+
+const showBookFetchHandler = bookId => {
+  fetch(`http:localhost:3000/api/v1/book/${bookId}`)
+  .then(response => response.json())
+  .then(data => {
+    $("#book-container").toggleClass("hide")
+    appendBookToPopup(data);
+  });
+};
+
+});//end document ready
